babe6160a07ca21712692841d63ce27d73f9e47d
chenbin <chenbin@cmhi.chinamobile.com>
2021-03-01 17:09:32 +0800

    fix ktlint

--- app/src/main/java/com/example/androiddevchallenge/DetailActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/DetailActivity.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
 import android.os.Bundle
@@ -5,7 +20,12 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
@@ -42,11 +62,13 @@ class DetailActivity : AppCompatActivity() {
     @Composable
     fun CatDetail(model: CatModel) {
         Column {
-            Image(modifier = Modifier
-                .clickable { finish() }
-                .padding(horizontal = 16.dp, vertical = 10.dp),
+            Image(
+                modifier = Modifier
+                    .clickable { finish() }
+                    .padding(horizontal = 16.dp, vertical = 10.dp),
                 painter = painterResource(id = R.drawable.nav_icon_back_black_nor),
-                contentDescription = "back")
+                contentDescription = "back"
+            )
             Column(
                 modifier = Modifier.padding(16.dp)
                     .verticalScroll(rememberScrollState())
